51e3f2855ed0d9b5c5be8a8aa42b8bb47b5c4578
Shuheng Zhang <zhangshuheng01@163.com>
2021-01-01 14:44:59 +0800

    Init Commit

---
+++ src/test/java/com/heng/reading/apiservice/ReadingSystemApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heng.reading.apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingSystemApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
