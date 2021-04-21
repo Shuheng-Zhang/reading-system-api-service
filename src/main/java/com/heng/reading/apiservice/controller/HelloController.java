bac8a741feb6d13c61b27a0942e056f53ba29271
Shuheng-Zhang <zhangshuheng01@163.com>
2021-04-21 13:49:01 +0800

    变更 用用系统测试用访问接口

--- src/main/java/com/heng/reading/apiservice/controller/HelloController.java
+++ src/main/java/com/heng/reading/apiservice/controller/HelloController.java
@@ -2,15 +2,17 @@ package com.heng.reading.apiservice.controller;
 
 import com.heng.reading.apiservice.comms.data.ResultData;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author heng
  */
+@RequestMapping("/test")
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public ResultData<String> hello() {
         return ResultData.success("Hello, World");
     }
